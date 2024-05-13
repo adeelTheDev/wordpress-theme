da83a92c155b0d6f719546aaad4ec748d5efe854
muhammadadeel-me <muhammadadeelcoder@gmail.com>
2024-05-13 23:41:53 +0500

    Move enqueue scrips from functions.php to blogify-theme class

--- functions.php
+++ functions.php
@@ -19,23 +19,3 @@ function blogify_get_theme_instance() {
 	\BLOGIFY_THEME\Inc\BLOGIFY_THEME::get_instance();
 }
 blogify_get_theme_instance();
-
-function blogify_enqueue_scripts() {
-
-	//	Register Styles
-	wp_register_style('blogify-stylesheet', GET_THEME_DIRECTORY_URI . '/assets/css/main.style.css', ['bootstrap'], filemtime(GET_THEME_DIRECTORY . '/assets/css/main.style.css'));
-	wp_register_style('bootstrap', GET_THEME_DIRECTORY_URI . '/assets/library/css/bootstrap.min.css', [], filemtime(GET_THEME_DIRECTORY . '/assets/library/css/bootstrap.min.css'));
-
-	// Register Scripts
-	wp_register_script('blogify-script', GET_THEME_DIRECTORY_URI . '/assets/js/main.script.js', '', filemtime(GET_THEME_DIRECTORY . '/assets/js/main.script.js'), true);
-
-	// Enqueue Styles
-	wp_enqueue_style('blogify-stylesheet');
-	wp_enqueue_style('bootstrap');
-
-	// Enqueue Scripts
-	wp_enqueue_script('blogify-script');
-
-}
-
-add_action('wp_enqueue_scripts', 'blogify_enqueue_scripts');
--- inc/classes/class-blogify-theme.php
+++ inc/classes/class-blogify-theme.php
@@ -13,12 +13,32 @@ class BLOGIFY_THEME {
 	use Singleton;
 
 	protected function __construct() {
-
 		// Load class.
-		$this->set_hooks();
+		$this->setup_hooks();
 	}
 
-	protected  function set_hooks() {
-
+	protected  function setup_hooks():void {
+		// Load hooks
+		add_action('wp_enqueue_scripts', [$this, 'enqueue_styles']);
+		add_action('wp_enqueue_scripts', [$this, 'enqueue_scripts']);
 	}
+
+	public  function enqueue_styles():void {
+		//	Register Styles
+		wp_register_style('blogify-stylesheet', GET_THEME_DIRECTORY_URI . '/assets/css/main.style.css', ['bootstrap'], filemtime(GET_THEME_DIRECTORY . '/assets/css/main.style.css'));
+		wp_register_style('bootstrap', GET_THEME_DIRECTORY_URI . '/assets/library/css/bootstrap.min.css', [], filemtime(GET_THEME_DIRECTORY . '/assets/library/css/bootstrap.min.css'));
+
+		// Enqueue Styles
+		wp_enqueue_style('blogify-stylesheet');
+		wp_enqueue_style('bootstrap');
+	}
+
+public  function enqueue_scripts():void {
+	// Register Scripts
+	wp_register_script('blogify-script', GET_THEME_DIRECTORY_URI . '/assets/js/main.script.js', '', filemtime(GET_THEME_DIRECTORY . '/assets/js/main.script.js'), true);
+
+	// Enqueue Scripts
+	wp_enqueue_script('blogify-script');
+}
+
 }
