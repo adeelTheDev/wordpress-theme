e383b0e6095d44618e753ddbcc3b3c7dad0546cc
muhammadadeel-me <muhammadadeelcoder@gmail.com>
2024-05-25 21:36:30 -0700

    Implement pagination for page breaks using wp_link_pages()

--- template-parts/components/blog/entry-content.php
+++ template-parts/components/blog/entry-content.php
@@ -8,20 +8,26 @@
 
 <div class="entry-content">
 	<?php
-	if( is_single() ){
+	if ( is_single() ) {
 		the_content(
 			sprintf(
-                wp_kses(
-                    __( 'Continue reading %s <span class="nav-meta">→</span>', BLOGIFY_TEXTDOMAIN ),
-                    [ 'span' => [ 'class' => [] ] ]
-                ),
-                the_title( '<span class="screen-reader-text">"', '"</span>', false )
+				wp_kses(
+					__( 'Continue reading %s <span class="nav-meta">→</span>', BLOGIFY_TEXTDOMAIN ),
+					[ 'span' => [ 'class' => [] ] ]
+				),
+				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 
-            )
+			)
 		);
+
+		wp_link_pages( [
+			'before' => '<div class="page-links">Pages:',
+			'after'  => '</div>',
+		] );
+
 	} else {
 		blogify_the_excerpt( '200' );
-        echo blogify_read_more();
+		echo blogify_read_more();
 	}
 	?>
 </div>
