52fc22934a21cbc576e38d128201d340bbfa1107
muhammadadeel-me <muhammadadeelcoder@gmail.com>
2024-05-25 21:17:17 -0700

    Optimize excerpt function and remove the link on featured image on single blog page

--- template-parts/components/blog/entry-header.php
+++ template-parts/components/blog/entry-header.php
@@ -7,10 +7,10 @@
 
 $the_post_id        = get_the_ID();
 $has_post_thumbnail = has_post_thumbnail();
-$the_post_link = get_the_permalink();
-$hide_page_title = get_post_meta( $the_post_id, '_hide_page_title', true);
-$title_class = ( ! empty( $hide_page_title ) && $hide_page_title === 'YES' ) ? 'd-none' : '';
-$feature_image_size = is_single() ? 'featured-large': 'featured-thumbnail';
+$the_post_link      = get_the_permalink();
+$hide_page_title    = get_post_meta( $the_post_id, '_hide_page_title', true );
+$title_class        = ( ! empty( $hide_page_title ) && $hide_page_title === 'YES' ) ? 'd-none' : '';
+$feature_image_size = is_single() ? 'featured-large' : 'featured-thumbnail';
 ?>
 <header class="entry-header">
 
@@ -20,17 +20,26 @@ $feature_image_size = is_single() ? 'featured-large': 'featured-thumbnail';
 	 * Display Featured Image
 	 */
 	if ( $has_post_thumbnail ) {
+		$thumbnail = get_the_post_custom_thumbnail(
+			$the_post_id,
+			$feature_image_size,
+			[
+				'class' => 'attachment attachment-featured-thumbnail featured-image '
+			]
+		);
 		?>
         <div class="entry-image mb-3">
-            <a href="<?php echo esc_url( $the_post_link ) ?>">
-                <?php the_post_custom_thumbnail(
-                    $the_post_id,
-                    $feature_image_size,
-                    [
-                        'class' => 'attachment attachment-featured-thumbnail featured-image '
-                    ]
-                ); ?>
-            </a>
+			<?php
+			if ( is_single() ) {
+				echo $thumbnail;
+			} else {
+				?>
+                <a href="<?php echo esc_url( $the_post_link ) ?>">
+					<?php echo $thumbnail; ?>
+                </a>
+				<?php
+			}
+			?>
         </div>
 		<?php
 	}
@@ -39,19 +48,19 @@ $feature_image_size = is_single() ? 'featured-large': 'featured-thumbnail';
 	 * Display Title
 	 */
 
-    if( is_single() || is_page() ) {
-        printf(
-            '<h1 class="page-title text-dark %1$s">%2$s</h1>',
-            esc_attr( $title_class ),
-            wp_kses_post( get_the_title() )
-        );
-    } else {
-	    printf(
-		    '<h2 class="entry-title fs-3 mb-3"><a href="%1$s" class="text-dark">%2$s</a></h2>',
-            esc_url( $the_post_link ),
-		    wp_kses_post( get_the_title() )
-	    );
-    }
+	if ( is_single() || is_page() ) {
+		printf(
+			'<h1 class="page-title text-dark %1$s">%2$s</h1>',
+			esc_attr( $title_class ),
+			wp_kses_post( get_the_title() )
+		);
+	} else {
+		printf(
+			'<h2 class="entry-title fs-3 mb-3"><a href="%1$s" class="text-dark">%2$s</a></h2>',
+			esc_url( $the_post_link ),
+			wp_kses_post( get_the_title() )
+		);
+	}
 	?>
 
 </header>
